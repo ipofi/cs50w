15c066e5dc00ef221fbe0f0d7812ef213420c036
Pofi Abel <peterabel07@gmail.com>
2021-09-01 13:58:27 +0100

    update

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("abel", views.abel, name="abel"),
+    path("david", views.david, name="david")
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
+
+def abel(request):
+    return HttpResponse("Hello, Abel!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
